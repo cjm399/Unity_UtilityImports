28601e332ed1da707a25a854538dc4741c48e52b
Chris McCole <chrismccole1@gmail.com>
2022-08-27 01:20:50 -0400

    Fix PlaceItemsInGrid to account for rectTransforms as well as regular gameobjects, some centering options as well.

--- Runtime/PlaceItemsInGrid.cs
+++ Runtime/PlaceItemsInGrid.cs
@@ -8,19 +8,61 @@ namespace Utilities
         public Vector3 initialGridPositionOffset = Vector3.zero;
         public float verticalSpacing = 10f;
         public float horizontalSpacing = 10f;
-        public int maxItemsPerLine = 10;
+        [Min(1)]
+        public uint maxItemsPerLine = 10;
+        public bool tryCenterHorizontal = false;
+        public bool tryCenterVertical = false;
+
+        private void OnValidate()
+        {
+            SpaceOutChildren();
+        }
 
         public void SpaceOutChildren()
         {
+            bool isCanvas = this.GetComponent<RectTransform>() != null;
+
             List<GameObject> children = gameObject.GetAllChildren();
 
             Vector3 initialPos = transform.position + initialGridPositionOffset;
+
+            if(isCanvas)
+            {
+                initialPos = GetComponent<RectTransform>().anchoredPosition;
+                initialPos += initialGridPositionOffset;
+            }
+
+            if(tryCenterHorizontal)
+            {
+                int numHor = Mathf.Min(children.Count, (int)maxItemsPerLine)-1;
+                float totalHorizontalSpacing = numHor * horizontalSpacing;
+                initialPos.x -= totalHorizontalSpacing / 2f;
+            }
+            
+            if (tryCenterVertical)
+            {
+                int numVer = (children.Count-1) / (int)maxItemsPerLine;
+                float totalVerticalSpacing = numVer * verticalSpacing;
+                initialPos.y -= totalVerticalSpacing / 2f;
+            }
+
             Vector3 currPos = initialPos;
             int itemsOnLine = 0;
 
             for (int childIndex = 0; childIndex < children.Count; ++childIndex)
             {
-                children[childIndex].transform.position = currPos;
+                if (isCanvas)
+                {
+                    RectTransform rt = children[childIndex].GetComponent<RectTransform>();
+                    if(rt != null)
+                    {
+                        rt.anchoredPosition = currPos;
+                    }
+                }
+                else
+                {
+                    children[childIndex].transform.position = currPos;
+                }
                 currPos.x += horizontalSpacing;
                 if (++itemsOnLine == maxItemsPerLine)
                 {
